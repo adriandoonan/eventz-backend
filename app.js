8b7d2ddb16f0b6a354aa040d29042b21a0c29f9c
Adrian Doonan <adriandoonan@gmail.com>
2024-03-14 12:49:50 +0100

    install json-server-auth and set up demo user and protected route

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 import morgan from "morgan";
 import dotenv from "dotenv";
 import jsonServer from "json-server";
+import auth from "json-server-auth";
 
 dotenv.config();
 //delay, id
 const server = jsonServer.create();
 const router = jsonServer.router("eventz-db.json");
 const middleware = jsonServer.defaults();
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT || 6969;
+
+const rules = auth.rewriter({
+	users: 660,
+});
+
+server.db = router.db;
 
 server.use(middleware);
+server.use(rules);
+server.use(auth);
 server.use(morgan("dev"));
 server.unsubscribe((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
